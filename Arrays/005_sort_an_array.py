c882543cbd325d33566debafeef9810223c85f5a
Yashwant Namdev <64881194+CodeCrafterYashwant@users.noreply.github.com>
2025-06-15 11:01:48 +0530

    Create 005_sort_an_array.py

---
+++ Arrays/005_sort_an_array.py
@@ -0,0 +1,7 @@
+# 🔍 Problem: https://leetcode.com/problems/sort-an-array/
+# 🔢 Difficulty: Easy
+# ⏱️ Runtime: 23 ms
+
+def sortArray(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        return nums
