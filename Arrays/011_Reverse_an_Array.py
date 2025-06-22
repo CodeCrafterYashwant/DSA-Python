f5e0ddd0ed04eb6e42b46c47f6a514d21591c53c
Yashwant Namdev <64881194+CodeCrafterYashwant@users.noreply.github.com>
2025-06-22 13:04:44 +0530

    Create 011_Reverse_an_Array.py

---
+++ Arrays/011_Reverse_an_Array.py
@@ -0,0 +1,11 @@
+# 🔍 Problem: https://www.geeksforgeeks.org/problems/reverse-an-array/1
+# 🔢 Difficulty: Easy
+# ⏱️ Time Taken: 0.19
+
+def reverseArray(self, arr):
+    # code here
+    arr.reverse()
+    return arr
+        
+        
+        
