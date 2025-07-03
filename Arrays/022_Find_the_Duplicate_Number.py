ae532dafe86ce98ac523223b48f4bf94d9bf5ffb
Yashwant Namdev <64881194+CodeCrafterYashwant@users.noreply.github.com>
2025-07-03 13:17:37 +0530

    Update 022_Find_the_Duplicate_Number.py

--- Arrays/022_Find_the_Duplicate_Number.py
+++ Arrays/022_Find_the_Duplicate_Number.py
@@ -7,14 +7,11 @@
 def findDuplicate(self, nums: List[int]) -> int:
         low = 1
         high = len(nums) - 1
-
         while low < high:
             mid = (low + high) // 2
             count = sum(num <= mid for num in nums)
-
             if count > mid:
                 high = mid
             else:
                 low = mid + 1
-
         return low
