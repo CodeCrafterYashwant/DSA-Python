6b53aeba83ba0424f1180b4385ad95cf590f31d6
Yashwant Namdev <64881194+CodeCrafterYashwant@users.noreply.github.com>
2025-07-03 10:07:50 +0530

    Update 021_Search_Insert_Position.py

--- Arrays/021_Search_Insert_Position.py
+++ Arrays/021_Search_Insert_Position.py
@@ -3,6 +3,21 @@
 # ⏱️ Runtime: 0 ms
 
 
+# Method = 1(Binary Search)
+def searchInsert(self, nums: List[int], target: int) -> int:
+        left ,right = 0,len(nums)-1
+        while left<= right:
+            mid = (left+right)//2
+            if nums[mid] == target:
+                return (mid)        
+            elif nums[mid]<target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return (left)
+
+
+# Method = 2
 def searchInsert(self, nums: List[int], target: int) -> int:
     for i in range(len(nums)):
         if nums[i] == target:
