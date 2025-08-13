09a869938b3a4435c7ea24a3f039251b07609ce3
Yashwant Namdev <64881194+CodeCrafterYashwant@users.noreply.github.com>
2025-08-13 21:58:40 +0530

    Update 011_Reverse_Linked_List_II.py

--- Linked Lists/011_Reverse_Linked_List_II.py
+++ Linked Lists/011_Reverse_Linked_List_II.py
@@ -1,8 +1,9 @@
 # 🔍 Problem: https://leetcode.com/problems/reverse-linked-list-ii/
 # 🔢 Difficulty: Easy
+
+
+# Method = 1
 # ⏱️ Runtime: 0 ms
-
-
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -20,3 +21,29 @@ class Solution:
             curr.val = v
             curr = curr.next
         return head
+    
+
+# Method = 2
+# ⏱️ Runtime: 0 ms
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if not head or left == right:
+            return head     
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy   
+        for _ in range(left - 1):
+            prev = prev.next     
+        curr = prev.next
+        next_node = None       
+        for _ in range(right - left):
+            next_node = curr.next
+            curr.next = next_node.next
+            next_node.next = prev.next
+            prev.next = next_node        
+        return dummy.next
