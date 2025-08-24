9b33ebee81dfdc2a8a91236aa3c8b0e4112be122
Yashwant Namdev <64881194+CodeCrafterYashwant@users.noreply.github.com>
2025-08-24 08:51:58 +0530

    Update 012_Remove_Duplicates_from_Sorted_List_II.py

--- Linked Lists/012_Remove_Duplicates_from_Sorted_List_II.py
+++ Linked Lists/012_Remove_Duplicates_from_Sorted_List_II.py
@@ -1,8 +1,9 @@
 # 🔍 Problem: https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
-# 🔢 Difficulty: Medium
+
+
+# Method = 1
 # ⏱️ Runtime: 0 ms
-
-
+# 🔢 Difficulty: Medium
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
@@ -20,4 +21,27 @@ class Solution:
                 curr.next = ListNode(temp.val)
                 curr = curr.next
             temp = temp.next
+        return dummy.next
+    
+
+# Method = 2
+# ⏱️ Runtime: 14 ms
+# 🔢 Difficulty: Medium
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        curr = head
+        data = []
+        while curr:
+            data.append(curr.val)
+            curr = curr.next
+        curr = head
+        unique = [x for x in data if data.count(x) == 1]
+        dummy = ListNode(0)
+        head = dummy
+        curr = head
+        for i in unique:
+            curr.next = ListNode(i)
+            curr = curr.next
         return dummy.next
