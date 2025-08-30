e1279fc831ac87d99995717a9746f9e62691ed85
Yashwant Namdev <64881194+CodeCrafterYashwant@users.noreply.github.com>
2025-08-30 18:05:14 +0530

    Update 003_Reverse_String.py

--- Recursion/003_Reverse_String.py
+++ Recursion/003_Reverse_String.py
@@ -1,8 +1,9 @@
 # 🔍 Problem: https://leetcode.com/problems/reverse-string/
-# 🔢 Difficulty: Easy
+
+
+# Method = 1
 # ⏱️ Runtime: 4 ms
-
-
+# 🔢 Difficulty: Easy
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         def helper(left,right):
@@ -11,3 +12,16 @@ class Solution:
             s[left],s[right] = s[right],s[left]
             helper(left+1,right-1)
         return helper(0,len(s)-1)
+
+
+# Method = 2
+# ⏱️ Runtime: 3 ms
+# 🔢 Difficulty: Easy
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        def helper(i):
+            if i>=len(s) // 2:
+                return
+            s[i],s[-i-1] = s[-i-1],s[i]
+            helper(i+1)
+        return helper(0)
