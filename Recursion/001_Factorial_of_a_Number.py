d31a228b638bcd0998eac0cc9bec1fb401e51f73
Yashwant Namdev <64881194+CodeCrafterYashwant@users.noreply.github.com>
2025-09-02 21:30:32 +0530

    Update 001_Factorial_of_a_Number.py

--- Recursion/001_Factorial_of_a_Number.py
+++ Recursion/001_Factorial_of_a_Number.py
@@ -1,10 +1,20 @@
 # 🔍 Problem: https://www.geeksforgeeks.org/problems/factorial5739/1
-# 🔢 Difficulty: Easy
+
+# Method = 1
 # ⏱️ Runtime: 0 ms
-
-
+# 🔢 Difficulty: Easy
 class Solution:
     def factorial (self, n):
         if n == 0:
             return 1
         return n * self.factorial(n-1)
+    
+
+# Method = 2
+# ⏱️ Runtime: 0 ms
+# 🔢 Difficulty: Medium
+class Solution:
+    def factorial(self, n, acc=1):
+        if n == 0:
+            return acc
+        return self.factorial(n-1, acc * n)
