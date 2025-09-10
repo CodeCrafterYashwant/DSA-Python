c9590166ebfb0f965913b9fdbb6d2e3a08f56791
Yashwant Namdev <64881194+CodeCrafterYashwant@users.noreply.github.com>
2025-09-10 18:29:17 +0530

    Update 001_Reverse_Linked_List.py

--- Linked Lists/001_Reverse_Linked_List.py
+++ Linked Lists/001_Reverse_Linked_List.py
@@ -1,8 +1,8 @@
 # 🔍 Problem: https://leetcode.com/problems/reverse-linked-list/
 # 🔢 Difficulty: Easy
+
+# Method = 1
 # ⏱️ Runtime: 0 ms
-
-
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -20,3 +20,24 @@ class Solution:
             curr = next_node
 
         return prev
+    
+# Method = 2
+# ⏱️ Runtime: 0 ms 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        numbers = []
+        curr = head
+        while curr:
+            numbers.append(curr.val)
+            curr = curr.next
+        numbers.reverse()
+        curr = head
+        for i in numbers:
+            curr.val = i
+            curr = curr.next
+        return head 
