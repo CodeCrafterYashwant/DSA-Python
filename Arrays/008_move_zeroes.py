2cb178479692f54e1b47f2206ea85d3928936c4f
Yashwant Namdev <64881194+CodeCrafterYashwant@users.noreply.github.com>
2025-09-13 22:01:43 +0530

    Update 008_move_zeroes.py

--- Arrays/008_move_zeroes.py
+++ Arrays/008_move_zeroes.py
@@ -1,8 +1,24 @@
 # 🔍 Problem: https://leetcode.com/problems/move-zeroes/
 # 🔢 Difficulty: Easy
+
+#Method = 1
+# ⏱️ Runtime: 3 ms
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        position = 0
+        for num in nums:
+            if num != 0:
+                nums[position] = num
+                position += 1
+        for i in range(position,len(nums)):
+            nums[i] = 0
+
+
+#Method = 2
 # ⏱️ Runtime: 617 ms
-
-
 def moveZeroes(self, nums: List[int]) -> None:
     """
     Do not return anything, modify nums in-place instead.
