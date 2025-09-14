1086f389339b2d6325570f673f2638716a92a400
Yashwant Namdev <64881194+CodeCrafterYashwant@users.noreply.github.com>
2025-09-14 21:24:04 +0530

    Update 004_rotate_array.py

--- Arrays/004_rotate_array.py
+++ Arrays/004_rotate_array.py
@@ -1,7 +1,29 @@
 # 🔍 Problem: https://leetcode.com/problems/rotate-array/
 # 🔢 Difficulty: Medium
-# ⏱️ Runtime: 1371 ms
 
+
+#Method = 1
+# ⏱️ Runtime: 3 ms
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n
+
+        def reverse(start,end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+        reverse(0,n-1)
+        reverse(0,k-1)
+        reverse(k,n-1)
+
+
+#Method = 2
+# ⏱️ Runtime: 1371 ms
 def rotate(self, nums: List[int], k: int) -> None:
     for i in range(k):
         nums.insert(0,nums[-1])
