3f3c432cb444a25d9f2c79fdf93a467defa4b0e5
Yashwant Namdev <64881194+CodeCrafterYashwant@users.noreply.github.com>
2025-09-15 23:04:05 +0530

    Update 005_sort_an_array.py

--- Arrays/005_sort_an_array.py
+++ Arrays/005_sort_an_array.py
@@ -1,7 +1,16 @@
 # 🔍 Problem: https://leetcode.com/problems/sort-an-array/
 # 🔢 Difficulty: Easy
-# ⏱️ Runtime: 23 ms
 
+
+# Method = 1
+# ⏱️ Runtime: 23 ms
 def sortArray(self, nums: List[int]) -> List[int]:
         nums.sort()
         return nums
+
+
+# Method = 2
+# ⏱️ Runtime: 18 ms
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return sorted(nums)
